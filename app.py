461808ec511dd6b9dd8fc9c303ebe23b14860314
1K3H <62631166+1K3H@users.noreply.github.com>
2020-07-07 05:39:17 +0900

    input값 DB에 넣기 test

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ db = client.mindmapmaker
 def home():
     return render_template('index.html')
 
+@app.route('/push')
+def push():
+    receive_test_input = request.form['give_test_input']
+    db.inputdata.insert_one({
+        'input':receive_test_input
+    })
+    return jsonify({'result':'success'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
