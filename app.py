39d02f877d30c277ae7f07b7422e35c9cdc644a8
1K3H <62631166+1K3H@users.noreply.github.com>
2020-07-09 13:57:37 +0900

    input 값 DB에 넣고 빼기

--- app.py
+++ app.py
@@ -12,13 +12,22 @@ db = client.mindmapmaker
 def home():
     return render_template('index.html')
 
-@app.route('/push')
-def push():
-    receive_test_input = request.form['give_test_input']
-    db.inputdata.insert_one({
-        'input':receive_test_input
+@app.route('/make', methods=['POST'])
+def save_node():
+    receive_from_input = request.form['give_from_input']
+    receive_to_input = request.form['give_to_input']
+    receive_edge_id = request.form['give_edge_id']
+    db.nodes.insert_one({
+        'from': receive_from_input,
+        'to': receive_to_input,
+        'edge': receive_edge_id
     })
     return jsonify({'result':'success'})
+
+@app.route('/load', methods=['GET'])
+def list_node():
+    nodes = list(db.nodes.find({}, {'_id':False}))
+    return jsonify({'result':'success', 'nodes':nodes})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
