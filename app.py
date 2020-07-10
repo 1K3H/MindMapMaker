b114d67037760899dea860ebcfb58708f734ef15
1K3H <62631166+1K3H@users.noreply.github.com>
2020-07-10 21:21:27 +0900

    delete_node 기능 구현

--- app.py
+++ app.py
@@ -29,5 +29,17 @@ def list_node():
     nodes = list(db.nodes.find({}, {'_id':False}))
     return jsonify({'result':'success', 'nodes':nodes})
 
+@app.route('/delete', methods=['POST'])
+def delete_node():
+    receive_from_input = request.form['give_from_input']
+    receive_to_input = request.form['give_to_input']
+    receive_edge_id = request.form['give_edge_id']
+    db.nodes.delete_one({
+        'from': receive_from_input,
+        'to': receive_to_input,
+        'edge': receive_edge_id
+    })
+    return jsonify({'result':'success'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
