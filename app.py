168064dda9dcd91a87864c6ac4046ebc9c48624a
1K3H <62631166+1K3H@users.noreply.github.com>
2020-07-10 21:54:44 +0900

    reset 기능 구현

--- app.py
+++ app.py
@@ -41,5 +41,10 @@ def delete_node():
     })
     return jsonify({'result':'success'})
 
+@app.route('/reset', methods=['POST'])
+def reset():
+    db.nodes.remove()
+    return jsonify({'result':'success'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
